541552bf10622a8113ce06c18e72a72e64897538
n4r3xDev <134960234+n4r3xDev@users.noreply.github.com>
2026-03-15 19:55:09 +0100

    Added some data warehouse functions

--- dags/datawarehouse/data_utils.py
+++ dags/datawarehouse/data_utils.py
@@ -1,12 +1,12 @@
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from pyscopg2.extras import RealDictCursor
+from psycopg2.extras import RealDictCursor
 
 table = "yt_api"
 
 def get_conn_cursor():
     hook = PostgresHook(postgres_conn_id="postgres_db_yt_elt", database="elt_db")
     conn = hook.get_conn()
-    cur = conn.cursor(cursfor_factory=RealDictCursor)
+    cur = conn.cursor(cursor_factory=RealDictCursor)
     return conn, cur
 
 def close_conn_cursor(conn, cur):
@@ -24,7 +24,7 @@ def create_table(schema):
     conn, cur = get_conn_cursor()
     if schema == 'staging':
         table_sql = f"""
-            REATE TABLE IF NOT EXISTS {schema}.{table} (
+            CREATE TABLE IF NOT EXISTS {schema}.{table} (
                 "Video_ID" VARCHAR(11) PRIMARY KEY NOT NULL,
                 "Video_Title" TEXT NOT NULL,
                 "Upload_Date" TIMESTAMP NOT NULL,
--- dags/main.py
+++ dags/main.py
@@ -2,6 +2,7 @@ from airflow import DAG
 import pendulum
 from datetime import datetime, timedelta
 from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
+from datawarehouse.dwh import staging_table, core_table
 
 local_tz = pendulum.timezone("Europe/Warsaw")
 
@@ -32,3 +33,15 @@ with DAG(
     save_to_json_task = save_to_json(extracted_data)
 
     playlist_id >> video_ids >> extracted_data >> save_to_json_task
+
+with DAG(
+    dag_id='update_db',
+    default_args=default_args,
+    description='DAG to process JSON file and insert data into both staging and core schemas',
+    schedule='0 15 * * *',
+    catchup=False,
+) as dag:
+    update_staging = staging_table()
+    update_core = core_table()
+
+    update_staging >> update_core
